ecc59b0cfc19f4ae198322e4f84ec05f0cefd473
semo0319 <sergei.mosiagin@netcracker.com>
2024-09-25 21:27:45 -0600

    - CompletableFuture

--- src/controller/Scheduler.java
+++ src/controller/Scheduler.java
@@ -9,84 +9,84 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Scheduler<T extends AbstractTask> {
 
+    private CompletableFuture<Void> addEvent = new CompletableFuture<Void>();
+    private CompletableFuture<Void> fireTimeEvent = new CompletableFuture<Void>();
+
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
-    private final Condition wCond = lock.writeLock().newCondition();
 
     private final BlockingQueue<T> queue = new PriorityBlockingQueue<>(Constants.CAPACITY);
 
     private final Executor pool =  Executors.newFixedThreadPool(Constants.THREADS);
 
-    private Thread subThread ;
 
     private boolean running = true;
 
     public void add(T entity) {
         lock.writeLock().lock();
         try {
-            this.queue.add(entity);
-            wCond.signalAll();
-
+            addEvent.thenRun(() -> {
+                    this.queue.add(entity);
+            }).join();
         } finally {
             lock.writeLock().unlock();
         }
     }
 
     public void start() {
-        subThread = new Thread(() -> {
-            while (running) {
-                executeDueTasks();
-            }
-        });
-        subThread.start();
+        while (running) {
+            CompletableFuture.anyOf(addEvent, fireTimeEvent)
+                    .thenRun(this::exetuteTasks)
+                    .thenRun(this::executeDueTasks);
+        }
     }
 
     public void stop() {
         lock.writeLock().lock();
         try {
             running = false;
-            wCond.signalAll();
         } finally {
             lock.writeLock().unlock();
         }
     }
 
     private void executeDueTasks() {
-
         lock.writeLock().lock();
-
         try {
             if(queue.isEmpty()) {
-                wCond.await();
+                fireTimeEvent.wait();
             } else {
-                exetuteTasks();
                 if(!queue.isEmpty()) {
-                    wCond.await(queue.peek().delay, TimeUnit.MILLISECONDS);
+                    fireTimeEvent.wait(queue.peek().delay);
                 }
             }
+            lock.writeLock().unlock();
         } catch (InterruptedException exception) {
             Thread.currentThread().interrupt();
-        } finally {
-            lock.writeLock().unlock();
         }
     }
 
     private void exetuteTasks() {
-        long currentTime = System.currentTimeMillis();
-        while(!queue.isEmpty()) {
-            if(!queue.peek().isPaused && currentTime >= queue.peek().executionTime) {
-                T entity = queue.poll();
-                pool.execute(entity);
-                if(entity.isRepeatable) {
-                    entity.executionTime += entity.delay;
-                    queue.add(entity);
+        lock.writeLock().lock();
+        try {
+            long currentTime = System.currentTimeMillis();
+            while(!queue.isEmpty()) {
+                if(!queue.peek().isPaused && currentTime >= queue.peek().executionTime) {
+                    T entity = queue.poll();
+                    pool.execute(entity);
+                    if(entity.isRepeatable) {
+                        entity.executionTime += entity.delay;
+                        queue.add(entity);
+                    }
+                    else {
+                        entity.isPaused = true;
+                    }
                 }
                 else {
-                    entity.isPaused = true;
+                    break;
                 }
             }
-            else {
-                break;
-            }
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 }
